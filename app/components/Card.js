348a1a17859d703eb9816509b062a3637abd6309
mishainx <fer.r@live.com.ar>
2023-11-25 13:00:20 -0300

    modificaciones

---
+++ app/components/Card.js
@@ -0,0 +1,10 @@
+
+import styles from './card.module.css'; // Asegúrate de tener el archivo de estilos Invitation.module.css
+
+export default function Card() {
+  return (
+    <div className={styles.cardContainer}>
+        asdadasd
+    </div>
+  );
+}
--- app/components/Invitation.js
+++ app/components/Invitation.js
@@ -1,24 +1,10 @@
-import Image from "next/image";
+import Card from './Card';
+import styles from './invitation.module.css'; // Asegúrate de tener el archivo de estilos Invitation.module.css
 
 export default function Invitation() {
-    return (
-        <div className="Invitation">
-            <div className="card">
-                    <Image
-                    src="/pokemones/pokemon-1.png"
-                    width={80}
-                    height={80}
-                    alt="Imagen Superior"
-                    className="Squirtle"
-                    >
-                    </Image>
-                <div className="card__face">
-                    <img src="/imagen-frontal.jpg" alt="Frontal" />
-                </div>
-                <div className="card__face card__face--back">
-                    <p>Hola, ¡te invitamos a la fiesta de cumpleaños!</p>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.invitationContainer}>
+        <Card/>
+    </div>
+  );
 }
