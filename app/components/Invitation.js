863240189623e2b705bd6fd2ad69ce8c87996c36
mishainx <fer.r@live.com.ar>
2023-11-25 10:49:36 -0300

    modificaciones

--- app/components/Invitation.js
+++ app/components/Invitation.js
@@ -1,13 +1,21 @@
+import Image from "next/image";
+
 export default function Invitation() {
     return (
         <div className="Invitation">
             <div className="card">
+                    <Image
+                    src="/pokemones/pokemon-1.png"
+                    width={80}
+                    height={80}
+                    alt="Imagen Superior"
+                    className="Squirtle"
+                    >
+                    </Image>
                 <div className="card__face">
-                    {/* Contenido frontal */}
                     <img src="/imagen-frontal.jpg" alt="Frontal" />
                 </div>
                 <div className="card__face card__face--back">
-                    {/* Contenido trasero, podría ser el texto de la invitación */}
                     <p>Hola, ¡te invitamos a la fiesta de cumpleaños!</p>
                 </div>
             </div>
