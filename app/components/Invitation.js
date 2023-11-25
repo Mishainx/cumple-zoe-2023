c10043e108247ac03a4c91ec2100144e5b041365
mishainx <fer.r@live.com.ar>
2023-11-25 10:25:55 -0300

    modificaciones

--- app/components/Invitation.js
+++ app/components/Invitation.js
@@ -1,8 +1,16 @@
 export default function Invitation() {
-
     return (
-        <div className={`Invitation`}>
-            <p>Hola, ¡te invitamos a la fiesta de cumpleaños!</p>
+        <div className="Invitation">
+            <div className="card">
+                <div className="card__face">
+                    {/* Contenido frontal */}
+                    <img src="/imagen-frontal.jpg" alt="Frontal" />
+                </div>
+                <div className="card__face card__face--back">
+                    {/* Contenido trasero, podría ser el texto de la invitación */}
+                    <p>Hola, ¡te invitamos a la fiesta de cumpleaños!</p>
+                </div>
+            </div>
         </div>
     );
 }
--- app/components/Pokebola.js
+++ app/components/Pokebola.js
@@ -34,7 +34,7 @@ export default function Pokebola() {
                     }}
                 />
             ) : showInvitation ? (
-                <Invitation />
+                <Invitation/>
             ) : null}
         </div>
     );
