f0a411018f0ff26912e431ef70112725b200458a
mishainx <fer.r@live.com.ar>
2023-11-26 14:48:48 -0300

    modificaciones

--- app/components/Invitation.js
+++ app/components/Invitation.js
@@ -4,25 +4,9 @@ import Image from 'next/image';
 export default function Invitation() {
   return (
     <div className={styles.invitationContainer}>
-      <div className={styles.nameContainer}>
-      <Image
-        src="/text/zoe-text.png"
-        width={150}
-        height={25}
-        layout="responsive"
-      >
-      </Image>
-      <Image
-        src="/text/text-6.png"
-        width={100}
-        height={25}
-        layout="responsive"
-      >
-      </Image>
-      </div>
 
       <Image
-        src="/text/text-1.png"
+        src="/text/text-unique.png"
         width={250}
         height={250}
         layout="responsive"
