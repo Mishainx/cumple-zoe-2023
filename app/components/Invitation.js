107aa8eaabd561b9cd643d9bd91ada6257fe4381
mishainx <fer.r@live.com.ar>
2023-11-26 14:28:33 -0300

    modificaciones

--- app/components/Invitation.js
+++ app/components/Invitation.js
@@ -1,27 +1,31 @@
-import Card from './Card';
 import styles from './invitation.module.css'; // Asegúrate de tener el archivo de estilos Invitation.module.css
 import Image from 'next/image';
 
 export default function Invitation() {
   return (
     <div className={styles.invitationContainer}>
+      <div className={styles.nameContainer}>
       <Image
         src="/text/zoe-text.png"
         width={150}
-        height={150}
-      >
-      </Image>
-
-      <Image
-        src="/text/text-1.png"
-        width={500}
-        height={500}
+        height={100}
+        layout="fixed"
       >
       </Image>
       <Image
         src="/text/text-6.png"
         width={100}
         height={100}
+        layout="fixed"
+      >
+      </Image>
+      </div>
+
+      <Image
+        src="/text/text-1.png"
+        width={500}
+        height={500}
+        layout="fixed"
       >
       </Image>
     </div>
